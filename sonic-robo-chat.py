002c7698ccfbffb4f3cd4fcf3a8ce38542d5f9e8
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2021-11-02 19:10:55 +0000

    Rewrite for update to TwitchIO 2.1.1

--- sonic-robo-chat.py
+++ sonic-robo-chat.py
@@ -5,8 +5,9 @@ import hashlib
 import random
 import yaml
 from typing import Dict, Optional
+from twitchio import Message, Chatter
 from twitchio.ext import commands
-from twitchio.dataclasses import User, Context, Message
+from twitchio.ext.commands import Context
 
 # Load config
 if len(sys.argv) > 1:
@@ -35,28 +36,12 @@ else:
 command_file = os.path.join(luafiles_dir, "chat_commands.txt")
 print(f"Using command file {command_file}")
 
-oauth_token = config['oauth_token']
-if not oauth_token.startswith("oauth:"):
-	oauth_token = "oauth:" + oauth_token
 
-command_prefix = config.get('command_prefix', "!")
-
-# Initialise bot
-bot = commands.Bot(
-	irc_token=oauth_token,
-	nick=config['bot_nick'],
-	prefix=command_prefix,
-	initial_channels=["#" + config['channel']]
-)
-
-
+# Global variables
 subscriber_only = config.get('subscriber_only', [])
 mod_only = config.get('mod_only', [])
 disabled = config.get('disabled', [])
 min_bits = config.get('min_bits', {})
-
-
-channel = None
 
 name_colour_list = ["pink", "yellow", "green", "blue", "red", "grey",
 	"orange", "sky", "purple", "aqua", "peridot", "azure", "brown", "rosy"]
@@ -82,15 +67,11 @@ name_colour_dictionary = {
 queue = []
 
 
-@bot.event
-async def event_ready():
-	print("Bot started.")
-	global bot, channel
-	channel = bot.get_channel(config['channel'])
-	if not channel:
-		print("Not connected to expected channel: " + config['channel'])
-	elif config.get('join_message'):
-		await channel.send(config['join_message'])
+oauth_token = config['oauth_token']
+if not oauth_token.startswith("oauth:"):
+	oauth_token = "oauth:" + oauth_token
+
+command_prefix = config.get('command_prefix', "!")
 
 
 def write_command(command_name: str, params: Dict[str, str] = None):
@@ -119,7 +100,7 @@ def write_command(command_name: str, params: Dict[str, str] = None):
 			queue = []
 
 
-def get_name_colour(author: User) -> str:
+def get_name_colour(author: Chatter) -> str:
 	global name_colour_list, name_colour_dictionary
 	colour = name_colour_dictionary.get(author.colour)
 	if not colour:
@@ -153,7 +134,7 @@ def bits_used(message: Message) -> int:
 
 
 def handle_command(name: str, context: Context) -> str:
-	print(f"received command {context.content}")
+	print(f"received command {context.command.full_name}")
 	global disabled, subscriber_only, mod_only, min_bits
 	if not context.author.is_mod:
 		if name in disabled:
@@ -169,259 +150,276 @@ def handle_command(name: str, context: Context) -> str:
 				return f"Command {name} needs {bits_needed} but {bits_received} were received."
 
 
-# Character commands
-@bot.command()
-async def char(ctx: Context):
-	error = handle_command('char', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	params = {}
-	if len(words) > 1:
-		if words[1].lower() == 'rcolour':
-			params['colour'] = 'random'
-		else:
-			params['character'] = words[1].lower()
-			if len(words) > 2 and words[2].lower() == 'rcolour':
-				params['colour'] = 'random'
-	write_command("CHARACTER", params)
+# Define bot
+class Bot(commands.Bot):
+	def __init__(self, token, prefix, channel):
+		super().__init__(token=token, prefix=prefix, initial_channels=[channel])
 
+	async def event_ready(self):
+		print(f'Logged into {self.connected_channels} as {self.nick}')
+		channel = bot.get_channel(config['channel'])
+		join_message = config.get('join_message')
+		if not channel:
+			print("Not connected to expected channel: " + config['channel'])
+		elif join_message:
+			print(f"Sending join message {join_message}")
+			await channel.send(join_message)
 
-@bot.command()
-async def ring(ctx: Context):
-	error = handle_command('ring', ctx)
-	if error is not None:
-		print(error)
-		return
-	bpr = parse_int(config.get('bits_per_ring', 0))
-	if bpr and bpr > 0:
-		bits_received = bits_used(ctx.message)
-		if bits_received > 0:
-			m = math.floor(bits_received / bpr)
-			for i in range(0, m):
-				write_command("RING")
+	# Character commands
+	@commands.command()
+	async def char(self, ctx: Context):
+		error = handle_command('char', ctx)
+		if error is not None:
+			print(error)
 			return
-	write_command("RING")
-
-
-@bot.command()
-async def unring(ctx: Context):
-	error = handle_command('unring', ctx)
-	if error is not None:
-		print(error)
-		return
-	bpu = parse_int(config.get('bits_per_unring', 0))
-	if bpu and bpu > 0:
-		bits_received = bits_used(ctx.message)
-		if bits_received > 0:
-			m = math.floor(bits_received / bpu)
-			for i in range(0, m):
-				write_command("UNRING")
-			return
-	write_command("UNRING")
-
-
-@bot.command(name='1up')
-async def oneup(ctx: Context):
-	error = handle_command('1up', ctx)
-	if error is not None:
-		print(error)
-		return
-	write_command("1UP")
-
-
-@bot.command()
-async def air(ctx: Context):
-	error = handle_command('air', ctx)
-	if error is not None:
-		print(error)
-		return
-	write_command("AIR")
-
-
-@bot.command()
-async def scale(ctx: Context):
-	error = handle_command('scale', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	if len(words) < 2:
-		print("scale command did not have an argument, ignoring")
-		return
-	char_scale = parse_float(words[1])
-	if char_scale is None:
-		print("scale was not a valid number, ignoring")
-		return
-	if char_scale < 0.1:
-		char_scale = 0.1
-	if char_scale > 10:
-		char_scale = 10
-	write_command("SCALE", {"scale": str(char_scale), "duration": 35 * 30})
-
-
-# Follower commands
-@bot.command()
-async def swap(ctx: Context):
-	error = handle_command('swap', ctx)
-	if error is not None:
-		print(error)
-		return
-	write_command("SWAP")
-
-
-@bot.command()
-async def foll(ctx: Context):
-	error = handle_command('foll', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	params = {}
-	if len(words) > 1:
-		if words[1].lower() == 'rcolour':
-			params['colour'] = 'random'
-		else:
-			params['character'] = words[1].lower()
-			if len(words) > 2 and words[2].lower() == 'rcolour':
+		words = ctx.message.content.split(' ')
+		params = {}
+		if len(words) > 1:
+			if words[1].lower() == 'rcolour':
 				params['colour'] = 'random'
-	write_command("FOLLOWER", params)
+			else:
+				params['character'] = words[1].lower()
+				if len(words) > 2 and words[2].lower() == 'rcolour':
+					params['colour'] = 'random'
+		write_command("CHARACTER", params)
+
+	@commands.command()
+	async def char(self, ctx: Context):
+		error = handle_command('char', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		params = {}
+		if len(words) > 1:
+			if words[1].lower() == 'rcolour':
+				params['colour'] = 'random'
+			else:
+				params['character'] = words[1].lower()
+				if len(words) > 2 and words[2].lower() == 'rcolour':
+					params['colour'] = 'random'
+		write_command("CHARACTER", params)
+
+	@commands.command()
+	async def ring(self, ctx: Context):
+		error = handle_command('ring', ctx)
+		if error is not None:
+			print(error)
+			return
+		bpr = parse_int(config.get('bits_per_ring', 0))
+		if bpr and bpr > 0:
+			bits_received = bits_used(ctx.message)
+			if bits_received > 0:
+				m = math.floor(bits_received / bpr)
+				for i in range(0, m):
+					write_command("RING")
+				return
+		write_command("RING")
+
+	@commands.command()
+	async def unring(self, ctx: Context):
+		error = handle_command('unring', ctx)
+		if error is not None:
+			print(error)
+			return
+		bpu = parse_int(config.get('bits_per_unring', 0))
+		if bpu and bpu > 0:
+			bits_received = bits_used(ctx.message)
+			if bits_received > 0:
+				m = math.floor(bits_received / bpu)
+				for i in range(0, m):
+					write_command("UNRING")
+				return
+		write_command("UNRING")
+
+	@commands.command(name='1up')
+	async def oneup(self, ctx: Context):
+		error = handle_command('1up', ctx)
+		if error is not None:
+			print(error)
+			return
+		write_command("1UP")
+
+	@commands.command()
+	async def air(self, ctx: Context):
+		error = handle_command('air', ctx)
+		if error is not None:
+			print(error)
+			return
+		write_command("AIR")
+
+	@commands.command()
+	async def scale(self, ctx: Context):
+		error = handle_command('scale', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		if len(words) < 2:
+			print("scale command did not have an argument, ignoring")
+			return
+		char_scale = parse_float(words[1])
+		if char_scale is None:
+			print("scale was not a valid number, ignoring")
+			return
+		if char_scale < 0.1:
+			char_scale = 0.1
+		if char_scale > 10:
+			char_scale = 10
+		write_command("SCALE", {"scale": str(char_scale), "duration": 35 * 30})
+
+	# Follower commands
+	@commands.command()
+	async def swap(self, ctx: Context):
+		error = handle_command('swap', ctx)
+		if error is not None:
+			print(error)
+			return
+		write_command("SWAP")
+
+	@commands.command()
+	async def foll(self, ctx: Context):
+		error = handle_command('foll', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		params = {}
+		if len(words) > 1:
+			if words[1].lower() == 'rcolour':
+				params['colour'] = 'random'
+			else:
+				params['character'] = words[1].lower()
+				if len(words) > 2 and words[2].lower() == 'rcolour':
+					params['colour'] = 'random'
+		write_command("FOLLOWER", params)
+
+	# Spawning object commands
+	@commands.command()
+	async def obj(self, ctx: Context):
+		error = handle_command('obj', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		if len(words) < 2:
+			print("Object command did not include an ID, ignoring")
+			return
+		object_id = words[1]
+		message = " ".join(words[2:])
+		colour = get_name_colour(ctx.author)
+		params = {"username": ctx.author.name, "namecolour": colour,
+			"message": message, "objectid": object_id}
+		write_command("OBJECT", params)
+
+	@commands.command()
+	async def badnik(self, ctx: Context):
+		error = handle_command('badnik', ctx)
+		if error is not None:
+			print(error)
+			return
+		message = " ".join(ctx.message.content.split(' ')[1:])
+		params = {"username": ctx.author.name,
+			"namecolour": get_name_colour(ctx.author), "message": message}
+		write_command("BADNIK", params)
+
+	@commands.command()
+	async def monitor(self, ctx: Context):
+		error = handle_command('monitor', ctx)
+		if error is not None:
+			print(error)
+			return
+		message = " ".join(ctx.message.content.split(' ')[1:])
+		params = {"username": ctx.author.name,
+			"namecolour": get_name_colour(ctx.author), "message": message}
+		write_command("MONITOR", params)
+
+	@commands.command()
+	async def spring(self, ctx: Context):
+		error = handle_command('spring', ctx)
+		if error is not None:
+			print(error)
+			return
+		colours = ['blue', 'yellow', 'red']
+		orientation = ['horizontal', 'vertical', 'diagonal']
+		direction = ['forward', 'back', 'left', 'right']
+		write_command("SPRING", {"colour": random.choice(colours),
+			"orientation": random.choice(orientation),
+			"direction": random.choice(direction)})
+
+	# Sound commands
+	@commands.command()
+	async def sfx(self, ctx: Context):
+		error = handle_command('sfx', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		if len(words) < 2:
+			print("sfx command did not have an argument, ignoring")
+			return
+		write_command("SOUND", {"sound": words[1]})
+
+	@commands.command()
+	async def bgm(self, ctx: Context):
+		error = handle_command('bgm', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		if len(words) < 2:
+			print("music command did not have an argument, ignoring")
+			return
+		write_command("MUSIC", {"track": words[1].upper()})
+
+	# Config commands
+	@commands.command(name='config')
+	async def config_command(self, ctx: Context):
+		error = handle_command('config', ctx)
+		if error is not None:
+			print(error)
+			return
+		words = ctx.message.content.split(' ')
+		if len(words) < 3:
+			print("config command did not have enough arguments, ignoring.")
+			return
+		write_command("CONFIG", {"setting": words[1], "value": words[2]})
+
+	@commands.command()
+	async def killall(self, ctx: Context):
+		error = handle_command('killall', ctx)
+		if error is not None:
+			print(error)
+			return
+		write_command("KILLALL")
+
+	@commands.command()
+	async def despawn(self, ctx: Context):
+		error = handle_command('despawn', ctx)
+		if error is not None:
+			print(error)
+			return
+		write_command("DESPAWN")
+
+	# Message handing
+	async def event_message(self, message: Message):
+		if message.author is None:
+			return
+		for character in "|^\r\n":  # Strip dangerous characters
+			message.content = message.content.replace(character, ' ')
+		await bot.handle_commands(message)
+		colour = get_name_colour(message.author)
+		if not config.get('display_chat_messages'):
+			return
+		global command_prefix
+		if message.content.startswith(command_prefix) and not config.get('display_chat_commands'):
+			return
+		if message.author.name == bot.nick and not config.get('display_bot_messages'):
+			return
+		write_command("CHAT", {"username": message.author.name, "message": message.content,
+		                       "namecolour": colour})
 
 
-# Spawning object commands
-@bot.command()
-async def obj(ctx: Context):
-	error = handle_command('obj', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	if len(words) < 2:
-		print("Object command did not include an ID, ignoring")
-		return
-	object_id = words[1]
-	message = " ".join(words[2:])
-	colour = get_name_colour(ctx.author)
-	params = {"username": ctx.author.name, "namecolour": colour,
-		"message": message, "objectid": object_id}
-	write_command("OBJECT", params)
-
-
-@bot.command()
-async def badnik(ctx: Context):
-	error = handle_command('badnik', ctx)
-	if error is not None:
-		print(error)
-		return
-	message = " ".join(ctx.content.split(' ')[1:])
-	params = {"username": ctx.author.name,
-		"namecolour": get_name_colour(ctx.author), "message": message}
-	write_command("BADNIK", params)
-
-
-@bot.command()
-async def monitor(ctx: Context):
-	error = handle_command('monitor', ctx)
-	if error is not None:
-		print(error)
-		return
-	message = " ".join(ctx.content.split(' ')[1:])
-	params = {"username": ctx.author.name,
-		"namecolour": get_name_colour(ctx.author), "message": message}
-	write_command("MONITOR", params)
-
-
-@bot.command()
-async def spring(ctx: Context):
-	error = handle_command('spring', ctx)
-	if error is not None:
-		print(error)
-		return
-	colours = ['blue', 'yellow', 'red']
-	orientation = ['horizontal', 'vertical', 'diagonal']
-	direction = ['forward', 'back', 'left', 'right']
-	write_command("SPRING", {"colour": random.choice(colours),
-		"orientation": random.choice(orientation),
-		"direction": random.choice(direction)})
-
-
-# Sound commands
-@bot.command()
-async def sfx(ctx: Context):
-	error = handle_command('sfx', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	if len(words) < 2:
-		print("sfx command did not have an argument, ignoring")
-		return
-	write_command("SOUND", {"sound": words[1]})
-
-
-@bot.command()
-async def bgm(ctx: Context):
-	error = handle_command('bgm', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	if len(words) < 2:
-		print("music command did not have an argument, ignoring")
-		return
-	write_command("MUSIC", {"track": words[1].upper()})
-
-
-# Config commands
-@bot.command(name='config')
-async def config_command(ctx: Context):
-	error = handle_command('config', ctx)
-	if error is not None:
-		print(error)
-		return
-	words = ctx.content.split(' ')
-	if len(words) < 3:
-		print("config command did not have enough arguments, ignoring.")
-		return
-	write_command("CONFIG", {"setting": words[1], "value": words[2]})
-
-
-@bot.command()
-async def killall(ctx: Context):
-	error = handle_command('killall', ctx)
-	if error is not None:
-		print(error)
-		return
-	write_command("KILLALL")
-
-
-@bot.command()
-async def despawn(ctx: Context):
-	error = handle_command('despawn', ctx)
-	if error is not None:
-		print(error)
-		return
-	write_command("DESPAWN")
-
-
-# Message handing
-@bot.event
-async def event_message(ctx: Context):
-	for character in "|^\r\n":  # Strip dangerous characters
-		ctx.content = ctx.content.replace(character, ' ')
-	await bot.handle_commands(ctx)
-	colour = get_name_colour(ctx.author)
-	if not config.get('display_chat_messages'):
-		return
-	global command_prefix
-	if ctx.content.startswith(command_prefix) and not config.get('display_chat_commands'):
-		return
-	if ctx.author.name == bot.nick and not config.get('display_bot_messages'):
-		return
-	write_command("CHAT", {"username": ctx.author.name, "message": ctx.content,
-		"namecolour": colour})
-
-
-if __name__ == "__main__":
-	bot.run()
+# Run bot
+bot = Bot(oauth_token, command_prefix, config['channel'])
+bot.run()
